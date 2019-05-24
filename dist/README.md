f5769e991e750b1b3df67f68840bc49b9530f835
Michael Yali <mihon4ik@gmail.com>
2019-05-24 17:10:06 +0300

    test(coverage): up to 100%

--- dist/README.md
+++ dist/README.md
@@ -3,6 +3,17 @@
 <div align="center">
   <strong>Async/await</strong> wrapper with error handling, retry and error override :purple_heart: :heart: :yellow_heart:
 </div>
+
+<div align="center">
+  <a href="https://travis-ci.org/zMotivat0r/oO.svg?branch=master">
+    <img src="https://img.shields.io/travis/zMotivat0r/oO/master.svg?style=flat-square"
+      alt="Build Status" />
+  </a>
+  <a href="https://codecov.io/github/zMotivat0r/oO">
+    <img src="https://img.shields.io/coveralls/github/zMotivat0r/oO.svg?style=flat-square"
+      alt="Test Coverage" />
+  </a>
+</div>
 <div align="center">
   <sub>Heavily inspired by <a href="https://github.com/scopsy/await-to-js">await-to-js</a></sub>
 </div>
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,4 +4,39 @@ describe('#oO', () => {
   it('should be a function', () => {
     expect(typeof oO).toBe('function');
   });
+
+  it('should return an error', async () => {
+    const call = async () => {
+      throw new Error();
+    };
+    const test = await oO(call());
+    expect(test[0]).toBeInstanceOf(Error);
+    expect(test[1]).toBeUndefined();
+  });
+
+  it('should return data', async () => {
+    const call = async () => 10;
+    const test = await oO(call());
+    expect(test[0]).toBeUndefined();
+    expect(test[1]).toBe(10);
+  });
+
+  it('should return an error with retry', async () => {
+    const call = async () => {
+      throw new Error();
+    };
+    const test = await oO(call(), { retry: 3 });
+    expect(test[0]).toBeInstanceOf(Error);
+    expect(test[1]).toBeUndefined();
+  });
+
+  it('should return custom error', async () => {
+    class TestError extends Error {}
+    const call = async () => {
+      throw new Error();
+    };
+    const test = await oO(call(), { err: new TestError() });
+    expect(test[0]).toBeInstanceOf(TestError);
+    expect(test[1]).toBeUndefined();
+  });
 });
